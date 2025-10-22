7dac60507ec700ccc09b8ea93a90a35bc2fa9953
Cauan Mendes <cauancunhamendes@gmail.com>
2025-10-22 18:46:33 -0300

    Adicionando e finalizando lista.

--- app/src/main/java/com/example/guiabairro/MainActivity.kt
+++ app/src/main/java/com/example/guiabairro/MainActivity.kt
@@ -24,15 +24,59 @@ class MainActivity : AppCompatActivity() {
         val estabelecimentos = listOf(
             Estabelecimento(
                 id = 1,
-                nome = getString(R.string.loja_nome),
-                tipo = getString(R.string.loja_tipo),
-                descricao = getString(R.string.loja_descricao),
-                telefone = "+5511999999999",
-                endereco = getString(R.string.loja_endereco),
-                site = "https://exemplo-loja.com",
-                horarioFuncionamento = getString(R.string.loja_horario),
-                imagemResId = android.R.drawable.ic_dialog_email
+                nome = getString(R.string.adgl_nome),
+                tipo = getString(R.string.adgl_tipo),
+                descricao = getString(R.string.adgl_descricao),
+                telefone = "(16) 3301-1996",
+                endereco = getString(R.string.adgl_endereco),
+                site = "https://comida.net.br/sobre/adgl---restaurante-ltda-araraquara-sp-31786958",
+                horarioFuncionamento = getString(R.string.adgl_horario),
+                imagemResId = R.drawable.adgl_restaurante
             ),
+            Estabelecimento(
+                id = 2,
+                nome = getString(R.string.amferreira_nome),
+                tipo = getString(R.string.amferreira_tipo),
+                descricao = getString(R.string.amferreira_descricao),
+                telefone = "(16) 3303-4216",
+                endereco = getString(R.string.amferreira_endereco),
+                site = "https://comida.net.br/sobre/am-ferreira-ltda-araraquara-sp-33491394",
+                horarioFuncionamento = getString(R.string.amferreira_horario),
+                imagemResId = R.drawable.alpine_restaurant
+            ),
+            Estabelecimento(
+                id = 3,
+                nome = getString(R.string.vivo_nome),
+                tipo = getString(R.string.vivo_tipo),
+                descricao = getString(R.string.vivo_descricao),
+                telefone = "(16) 99621-9668",
+                endereco = getString(R.string.vivo_endereco),
+                site = "https://lojas.vivo.com.br/loja-vivo-jardim-bandeirantes-araraquara-sp",
+                horarioFuncionamento = getString(R.string.vivo_horario),
+                imagemResId = R.drawable.vivo
+            ),
+            Estabelecimento(
+                id = 4,
+                nome = getString(R.string.passarinho_nome),
+                tipo = getString(R.string.passarinho_tipo),
+                descricao = getString(R.string.passarinho_descricao),
+                telefone = "(16) 3331-6677",
+                endereco = getString(R.string.passarinho_endereco),
+                site = "https://passarinhohortifruti.com.br",
+                horarioFuncionamento = getString(R.string.passarinho_horario),
+                imagemResId = R.drawable.passarinho
+            ),
+            Estabelecimento(
+                id = 5,
+                nome = getString(R.string.aiko_nome),
+                tipo = getString(R.string.aiko_tipo),
+                descricao = getString(R.string.aiko_descricao),
+                telefone = "(16) 3333-8899",
+                endereco = getString(R.string.aiko_endereco),
+                site = "https://empresadois.com.br/cnpj/aiko-12398481000114",
+                horarioFuncionamento = getString(R.string.aiko_horario),
+                imagemResId = R.drawable.aiko
+            )
         )
 
         estabelecimentoAdapter = EstabelecimentoAdapter(this, estabelecimentos)
